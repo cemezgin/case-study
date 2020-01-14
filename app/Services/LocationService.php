558bd71beaba00a6c03b951c0eb54e18db9b5757
Cem Ezgin <cem.ezgin@metglobal.com>
2020-01-14 21:36:05 +0300

    Add LocationService

---
+++ app/Services/LocationService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service;
+
+use App\Location;
+use App\Service\Interfaces\LocationServiceInterface;
+
+class LocationService
+{
+    public function save($request)
+    {
+        $location = new Location;
+        $location = $this->map($location, $request);
+
+        return $location;
+    }
+
+    /**
+     * @param $request
+     * @param $location Location
+     * @return mixed
+     */
+    public function update($request, $location)
+    {
+        $location = $this->map($location, $request);
+        return $location->save();
+    }
+
+    /**
+     * @param $location
+     * @param $request
+     * @return Location
+     */
+    public function map($location, $request)
+    {
+        $location->city = $request['city'];
+        $location->state = $request['state'];
+        $location->country = $request['country'];
+        $location->zip_code = $request['zip_code'];
+        $location->address = $request['address'];
+
+        return $location;
+    }
+}
